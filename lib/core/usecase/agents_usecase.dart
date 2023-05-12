ec642f1c17593cd1b3cb8ed158498aac695608a4
Emerson Soares <84353395+EmersonAD@users.noreply.github.com>
2023-05-12 13:58:40 -0300

    agents usecase finished

--- lib/core/usecase/agents_usecase.dart
+++ lib/core/usecase/agents_usecase.dart
@@ -1,10 +1,29 @@
+import 'package:flutter_application/data/repository/valorant_repository.dart';
+
+import '../../data/model/agents_response_entity.dart';
+
 abstract class AgentsUseCase {
-    List<AgentsResponseData> getAgents();
+  List<AgentsResponseData> getAgents();
 }
 
 class AgentsUseCaseImpl implements AgentsUseCase {
-    final AgentsRepository repository;
+  final AgentsRepository _repository;
 
-    AgentsUseCaseImpl(this.repository);
+  AgentsUseCaseImpl(this._repository);
 
+  @override
+  List<AgentsResponseData> getAgents() {
+    List<AgentsResponseData> agentsFilltered = [];
+
+    _repository.getAgents().then((value) {
+      List<AgentsResponseData> agents = value.data;
+
+      for (AgentsResponseData agent in agents) {
+        if (agent.isPlayableCharacter == true) {
+          agentsFilltered.add(agent);
+        }
+      }
+    });
+    return agentsFilltered;
+  }
 }
